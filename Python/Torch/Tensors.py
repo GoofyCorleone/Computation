2e40446ea445fbf0a5ef80aced294236c3c3de60
JafertSerrano <miguel2190742@correo.uis.edu.co>
2025-12-18 20:20:37 -0500

    Pytorch basis

---
+++ Python/Torch/Tensors.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+n = np.linspace(0,1,5)
+t = torch.linspace(0,1,5)
+
+#Can be resize
+
+n = np.arange()
